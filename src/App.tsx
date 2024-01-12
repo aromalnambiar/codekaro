15f7f4381435873a08f703345eda4e4a04414dae
aromalnambiar <aromalndr@gmail.com>
2024-01-12 09:00:46 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import {Router, Route} from 'react-router-dom'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='mt-10 ' >
+        p
+      </div>
+      
+    </>
+  )
+}
+
+export default App
